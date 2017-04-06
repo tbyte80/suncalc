ce32a5b77d7d8e7e50f8e3759066fc469aa9af1b
Arian <arian.tan@gmx.de>
2017-04-06 11:40:36 +0200

    Some improvements in return values and fixes

--- suncalc.js
+++ suncalc.js
@@ -23,11 +23,11 @@ var PI   = Math.PI,
 // date/time constants and conversions
 
 var dayMs = 1000 * 60 * 60 * 24,
-    J1970 = 2440588,
+    J1970 = 2440587.5,
     J2000 = 2451545;
 
-function toJulian(date) { return date.valueOf() / dayMs - 0.5 + J1970; }
-function fromJulian(j)  { return new Date((j + 0.5 - J1970) * dayMs); }
+function toJulian(date) { return date.valueOf() / dayMs - (date.getTimezoneOffset()/1440) + J1970; }
+function fromJulian(j)  { return new Date((j - J1970) * dayMs); }
 function toDays(date)   { return toJulian(date) - J2000; }
 
 
@@ -124,13 +124,21 @@ function julianCycle(d, lw) { return Math.round(d - J0 - lw / (2 * PI)); }
 function approxTransit(Ht, lw, n) { return J0 + (Ht + lw) / (2 * PI) + n; }
 function solarTransitJ(ds, M, L)  { return J2000 + ds + 0.0053 * sin(M) - 0.0069 * sin(2 * L); }
 
-function hourAngle(h, phi, d) { return acos((sin(h) - sin(phi) * sin(d)) / (cos(phi) * cos(d))); }
+function hourAngle(h, phi, d) {
+    var arg = (sin(h) - sin(phi) * sin(d)) / (cos(phi) * cos(d));
+    if(arg > 1)  return -Infinity;
+    if(arg < -1) return  Infinity;
+    return acos(arg);
+}
 
 // returns set time for the given sun altitude
 function getSetJ(h, lw, phi, dec, n, M, L) {
 
-    var w = hourAngle(h, phi, dec),
-        a = approxTransit(w, lw, n);
+    var w = hourAngle(h, phi, dec);
+    if(!isFinite(w))
+        return w;
+
+    var a = approxTransit(w, lw, n);
     return solarTransitJ(a, M, L);
 }
 
@@ -164,10 +172,14 @@ SunCalc.getTimes = function (date, lat, lng) {
         time = times[i];
 
         Jset = getSetJ(time[0] * rad, lw, phi, dec, n, M, L);
+
+        if(!isFinite(Jset))
+            Jnoon = 0;
+
         Jrise = Jnoon - (Jset - Jnoon);
 
-        result[time[1]] = fromJulian(Jrise);
-        result[time[2]] = fromJulian(Jset);
+        result[time[1]] = !isFinite(Jrise)? Jrise < 0 : fromJulian(Jrise);
+        result[time[2]] = !isFinite(Jset)? Jrise < 0 : fromJulian(Jset);
     }
 
     return result;
