2229883a71c13e764a8e3e3adf01cfff6d3d4d1b
Arian <arian.tan@gmx.de>
2017-06-21 11:23:38 +0200

    Update suncalc.js
    removed wrong offset calculation

--- suncalc.js
+++ suncalc.js
@@ -26,7 +26,7 @@ var dayMs = 1000 * 60 * 60 * 24,
     J1970 = 2440587.5,
     J2000 = 2451545;
 
-function toJulian(date) { return date.valueOf() / dayMs - (date.getTimezoneOffset()/1440) + J1970; }
+function toJulian(date) { return date.valueOf() / dayMs + J1970; }
 function fromJulian(j)  { return new Date((j - J1970) * dayMs); }
 function toDays(date)   { return toJulian(date) - J2000; }
 
